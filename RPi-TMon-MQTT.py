48362c2449467a83d3ad9d6387bb15276339d16e
AxReds <alessior@live.com>
2022-11-29 09:12:46 +0100

    comments added

--- RPi-TMon-MQTT.py
+++ RPi-TMon-MQTT.py
@@ -1,4 +1,26 @@
+#!/usr/bin/env python3
+#to make the script executable added the above line (a shebang: a # + a !)
+#read the readme file for details
 
+#Raspberry-Pi Temperature Monitoring which publishes data to MQTT-Brocker (RPi-TMon-MQTT) - v1.0.
+#Copyright (C) 2020 Alessio Rossini <alessior@live.com>
+#Original source code available at https://github.com/AxReds/RPi-TMon-MQTT
+
+#
+#This program is free software; you can redistribute it and/or modify it under
+#the terms of the GNU General Public License as published by the Free Software Foundation;
+#either version 3 of the License, or any later version.
+#
+#This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+#without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#See the GNU General Public License for more details
+#https://opensource.org/
+#
+#You should have received a copy of the GNU General Public License along with this program; 
+#if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+#
+#
+#
 
 # import
 import time
